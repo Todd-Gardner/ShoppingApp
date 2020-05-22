d3a89876ce68dcf2ff9d2915824da9510e54a421
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2020-05-22 14:10:15 -0400

    Added Product class and added dummy-data to use

---
+++ models/product.js
@@ -0,0 +1,12 @@
+// What the product (structure) should look like
+
+class Product {
+    constructor (id, ownerId, title, imageUrl, description, price) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+}
