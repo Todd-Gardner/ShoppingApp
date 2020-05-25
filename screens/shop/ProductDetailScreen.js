a2aa271dc41b08dfc749a917d6b7ef6fe574e71d
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2020-05-25 11:13:54 -0400

    Setting up ProductDetailScr- Saving before update

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -11,6 +11,7 @@ import { useSelector } from "react-redux"; //to get product from store
 
 const ProductDetailScreen = (props) => {
   const productId = props.navigation.getParam("productId");
+
   //get the single product
   const selectedProduct = useSelector((state) =>
     state.products.availableProducts.find((product) => product.id === productId)
@@ -18,8 +19,16 @@ const ProductDetailScreen = (props) => {
 
   return (
     <ScrollView>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{ uri: selectedProduct.imageUrl }}
+        />
+        <Button title="Add to Cart" onPress={() => {}} />
+      </View>
       <View>
-        <Text>{selectedProduct.title}</Text>
+        <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+        <Text style={styles.description}>{selectedProduct.description}</Text>
       </View>
     </ScrollView>
   );
@@ -28,10 +37,29 @@ const ProductDetailScreen = (props) => {
 // Show title in navigation header
 ProductDetailScreen.navigationOptions = (navData) => {
   return {
-    headerTitle: navData.navigation.getParam("productTitle")
+    headerTitle: navData.navigation.getParam("productTitle"),
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: "100%",
+    height: 300,
+    //overflow: 'hidden'
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    //overflow: "hidden"
+  },
+  price: {
+      paddingVertical: 50,
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#888',
+      textAlign: "center"
+    },
+  description: {}
+});
 
 export default ProductDetailScreen;
